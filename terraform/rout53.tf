679909a29be9f372710f9d4abeb26710050bbeec
Mohammed Sayed <mohammedsayed9607@gmail.com>
2024-09-18 15:41:12 +0100

    Updated to add route 53 creation + deletion

--- terraform/rout53.tf
+++ terraform/rout53.tf
@@ -2,14 +2,11 @@ data "aws_route53_zone" "lab_zone" {
   name = "lab.mohammedsayed.com"
 }
 
-resource "aws_route53_record" "tm_alias_record" {
+resource "aws_route53_record" "tm_cname_record" {
   zone_id = data.aws_route53_zone.lab_zone.id  
   name    = "tm.lab.mohammedsayed.com"
-  type    = "A"
-
-  alias {
-    name                   = aws_lb.tm_alb.dns_name  
-    zone_id                = aws_lb.tm_alb.zone_id   
-    evaluate_target_health = false
-  }
+  type    = "CNAME"
+  
+  ttl     = 300
+  records = [aws_lb.tm_alb.dns_name] 
 }
